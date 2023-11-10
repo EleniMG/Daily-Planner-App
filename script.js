2826fcdfc1fc258af68e98d139e34b77df275b68
Eleni <elenimariex@gmail.com>
2023-11-10 18:57:29 +0000

    Dynamically creating a table by creating each row and each column for the time, button, and input

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 var currentTime = dayjs().format("dddd Do MMMM YYYY");
 $('#currentDay').text(currentTime);
+
+var times = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+var timetableContainer = $('.container');
+
+
+
+/* <div class="col-2">col</div>
+<div class="col-8">col</div>
+<div class="col-2"><button type="button" class="btn btn-primary"></button></div>*/
+
+
+function createTimetableRows(){
+    for (var i = 0; i < times.length; i++){
+        timetableContainer.append('<div></div>').children().addClass('row');
+    }
+}
+
+createTimetableRows()
+
+
+var timeColumn = $('<div>').addClass("col-2");
+var inputColumn = $('<input>').addClass("col-8");
+var buttonColumn = $('<button>').addClass("col-2 col-btn btn-primary");
+var timetableRow = $('.row');
+
+timetableRow.append(timeColumn,inputColumn,buttonColumn)
