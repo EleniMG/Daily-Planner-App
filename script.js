8a3e3ab109309fe4ef4dd0650052a5b0f0fc63e7
Eleni <elenimariex@gmail.com>
2023-11-11 00:07:05 +0000

    Adding text and CSS pseudoclass to save button

--- script.js
+++ script.js
@@ -5,12 +5,7 @@ var times = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 var timetableContainer = $('.container');
 
 
-
-/* <div class="col-2">col</div>
-<div class="col-8">col</div>
-<div class="col-2"><button type="button" class="btn btn-primary"></button></div>*/
-
-
+// Creating as many rows as there are hour entries
 function createTimetableRows(){
     for (var i = 0; i < times.length; i++){
         timetableContainer.append('<div></div>').children().addClass('row');
@@ -19,7 +14,7 @@ function createTimetableRows(){
 
 createTimetableRows()
 
-
+// Compiling Bootstrap grid columns using jQuery
 var timeColumn = $('<div>').addClass("hour col-2");
 var inputColumn = $('<input>').addClass("description textarea col-8");
 var buttonColumn = $('<button>').addClass("saveBtn col-2 col-btn btn-primary");
@@ -27,7 +22,10 @@ var timetableRow = $('.row');
 
 timetableRow.append(timeColumn,inputColumn,buttonColumn)
 
+// Adding the hours to the schedule
 $('.hour').each(function(index){
     $(this).text(times[index])
     }
 )
+
+$('.saveBtn').text("Save")
